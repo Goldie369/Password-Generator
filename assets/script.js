ec68cc87e0a91ce92ec95adce0e4b78f00dd4b3c
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-02-11 10:23:25 -0600

    adding a function plus if else statement

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,18 @@ const numberSet = ["0","1","2","3","4","5","6","7","8","9"]
 const symbols = ["!","#","$","%","&","'","(",'"',")","*",",","+","-",".","/",":",";","<","=",">","?","@","[","","]","^","_","`","{","|","}","~"]
  // Added a symbols in order to be used for the password generator//
  
+
+ // Adding a function//
+ function generatePassword() {
+    // Adding a if/else statement
+       charNum = prompt("How long would you like your password to be? (between 8 and 128 characters)");
+       if (charNum < 8 || charNum > 128) {
+         alert("Password must be between 8 and 128 characters!");
+         return;
+       } else if (isNaN(charNum)) {
+         alert("Invalid entry");
+         return;
+       }
+       else {
+        alert("Password length set!");
+        
