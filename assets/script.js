639c78850a2932e63bf3f7adfd92b3459a97b79c
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-02-11 10:00:41 -0600

    adding a symboils to the script

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var generateBtn = document.querySelector("generate");
+
+const upperCaseSet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const lowerCaseSet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const numberSet = ["0","1","2","3","4","5","6","7","8","9"]
+const symbols = ["!","#","$","%","&","'","(",'"',")","*",",","+","-",".","/",":",";","<","=",">","?","@","[","","]","^","_","`","{","|","}","~"]
+ // Added a symbols in order to be used for the password generator//
+ 
